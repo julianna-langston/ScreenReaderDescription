10a2dcd69603d40a9137ee241445a71bbad5265d
Julianna Langston <julie.langston@gmail.com>
2024-09-10 00:37:43 -0400

    Generate list

---
+++ lister.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import fs from "fs";
+import type { ScriptInfo } from "./types";
+
+const directory = path.join(__dirname, "scripts");
+const files = fs.readdirSync(directory);
+const listing = files.map((f) => {
+    const contents: ScriptInfo = JSON.parse(fs.readFileSync(path.join(directory, f)).toString());
+
+    return {
+        ...contents.source,
+        ...contents.metadata,
+        file: `scripts/${f}`,
+        languages: contents.scripts.map(({language}) => language)
+    };
+});
+
+fs.writeFileSync("./listing.json", JSON.stringify(listing));
+console.log("Done");
