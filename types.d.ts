5d46047662e268802926fd479c49b0011664e3c6
Julianna Langston <julie.langston@gmail.com>
2024-09-10 00:29:58 -0400

    Arrange types

--- types.d.ts
+++ types.d.ts
@@ -1,23 +1,34 @@
-export interface ScriptData {
-    source: {
-        url: string;
-        domain?: "youtube";
-        id?: string;
-    };
-    metadata: {
-        title: string;
-        creator?: string;
-        seriesTitle?: string;
-        season?: number;
-        episode?: number;
-        type: "music video" | "television episode" | "movie" | "other";
-    };
-    scripts: {
-        language: string;
-        author: string;
-        tracks: {
-            text: string;
-            timestamp: number;
-        }[];
+export interface ScriptInfo {
+    source: ScriptSource;
+    metadata: ScriptMetadata;
+    scripts: ScriptData[];
+}
+
+interface ScriptSource {
+    url: string;
+    domain?: "youtube";
+    id?: string;
+}
+
+interface ScriptMetadata {
+    title: string;
+    creator?: string;
+    seriesTitle?: string;
+    season?: number;
+    episode?: number;
+    type: "music video" | "television episode" | "movie" | "other";
+}
+
+interface ScriptData {
+    language: HTMLElement["lang"];
+    author: string;
+    tracks: {
+        text: string;
+        timestamp: number;
     }[];
 }
+
+export type ScriptListing = {
+    "languages": ScriptData["language"][];
+    file: string;
+} & ScriptMetadata & ScriptSource;
