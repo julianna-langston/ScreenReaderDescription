04c34711f8b26a0d685958bad174276a27b3e393
Julianna Langston <julie.langston@gmail.com>
2024-09-22 15:42:46 -0400

    'Recently Added' section to lister

--- lister.ts
+++ lister.ts
@@ -6,10 +6,12 @@ type ListType = {
     id: ScriptInfo["source"]["id"];
     title: string;
 };
+type ListTypeList = Array<ListType>;
 
 const directory = path.join(__dirname, "transcripts");
 const files = fs.readdirSync(directory);
-const listing: Partial<Record<SupportedVideoTypes, Array<ListType>>> = {};
+const listing: Partial<Record<SupportedVideoTypes, ListTypeList>> = {};
+const recentlyAdded: ListTypeList = [];
 
 function toSortedByProperty<T>(arr: T[], key: keyof T): T[]{
     // @ts-expect-error: VSCode doesn't know about Array.toSorted
@@ -49,21 +51,41 @@ const generateTitle = ({type, title, creator, seriesTitle, season, episode}: Scr
     }
 }
 
+const cutOffPoint = new Date().valueOf() - (1000 * 60 * 60 * 24 * 7);   // 7 days ago
+
 files.map((f) => {
+    const stats = fs.statSync(path.join(directory, f));
     const {source, metadata}: ScriptInfo = JSON.parse(fs.readFileSync(path.join(directory, f)).toString());
     
     if(metadata.draft || metadata.requiresExtension){
         return;
     }
 
-    listing[metadata.type] ??= [];
-    listing[metadata.type]?.push({
+    const metadataBundle = {
         id: source.id,
         title: generateTitle(metadata)
-    });
+    };
+
+    listing[metadata.type] ??= [];
+    listing[metadata.type]?.push(metadataBundle);
+
+    if(stats.birthtimeMs > cutOffPoint){
+        recentlyAdded.push(metadataBundle);
+    }
 });
 
 const title = "Description Listing";
+
+const displayType: Record<SupportedVideoTypes, string> = {
+    "movie": "Movie",
+    "music video": "Music Video",
+    "other": "Other",
+    "television episode": "Television Episode"
+};
+
+const printListItems = (list: ListTypeList) => list.map(({id, title}) => {
+    return `<li><a href="./youtube_embed.html?id=${id}">${title}</a></li>`;
+}).join("\n\t\t\t");
 
 const html = `
 <!doctype html>
@@ -73,14 +95,18 @@ const html = `
     </head>
     <body>
         <h1>${title}</h1>
-        ${Object.entries(listing).map(([type, scripts]) => {
-            return `
-        <h2>${type}</h2>
+        <h2>Recently Added</h2>
 
         <ul>
-            ${toSortedByProperty<ListType>(scripts, "title").map(({id, title}) => {
-                return `<li><a href="./youtube_embed.html?id=${id}">${title}</a></li>`;
-            }).join("\n\t\t\t")}
+            ${printListItems(recentlyAdded)}
+        </ul>
+
+        ${Object.entries(listing).map(([type, scripts]) => {
+            return `
+        <h2>${displayType[type as SupportedVideoTypes]}</h2>
+
+        <ul>
+            ${printListItems(toSortedByProperty<ListType>(scripts, "title"))}
         </ul>`;
         }).join("\n\n")}
     </body>
