03dcdb21fa7aebddd799bac92a8420ff6c8b0796
Julianna Langston <julie.langston@gmail.com>
2025-06-20 20:32:25 -0400

    Remove unused function

--- editor/src/bridge_functions.ts
+++ editor/src/bridge_functions.ts
@@ -3,7 +3,3 @@ import { ScriptData } from "./types";
 export const sendMessageUpdateTracks = (tracks: ScriptData["tracks"]) => {
     document.dispatchEvent(new CustomEvent("ScreenReaderDescription-Track-Update", {detail: {tracks}}));
 }
-
-export const sendMessageUpdatePlayback = (playPause: boolean, currentTime: number) => {
-    document.dispatchEvent(new CustomEvent("ScreenReaderDescription-Playback-Update", {detail: {playPause, currentTime}}));
-}
