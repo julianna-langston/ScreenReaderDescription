ddcecf4035ec089c60842dbd383c110357992dfa
Julianna Langston <julie.langston@gmail.com>
2026-01-01 17:31:10 -0500

    Update catalog generation

--- generate-catalog.ts
+++ generate-catalog.ts
@@ -42,7 +42,7 @@ function scanTranscriptsDirectory(): CatalogEntry {
       if (stats.isDirectory()) {
         scanDir(itemPath, relPath);
       } else if (stats.isFile() && item.endsWith('.json')) {
-        processJsonFile(itemPath, `/transcripts/${relPath}`);
+        processJsonFile(itemPath, `/${dir}/${relPath}`);
       }
     }
   }
